fce410b8765092b6e2cb9e5d1c2ffab92fdcf69b
yonatanf21-meet <yonatanf21@meet.mit.edu>
2019-07-30 09:56:10 -0400

    Stopping here

--- snake.py
+++ snake.py
@@ -42,7 +42,64 @@ for loop in range(START_LENGTH):
     snake.goto(x_pos,y_pos)
     new_stamp()
 
+def remove_tail():
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+    
+snake.direction = "Up"
+
+def up():
+    snake.direction="Up"
+    move_snake()
+    print("you pressed the up key!")
+def down():
+    snake.direction="Down"
+    move_snake()
+    print("you pressed the down key!")
+def right():
+    snake.direction="Right"
+    move_snake()
+    print("you pressed the right key!")
+def left():
+    snake.direction="Left"
+    move_snake()
+    print("you pressed the left key!")
+
+
+turtle.onkeypress(up, "Up")
+turtle.onkeypress(down, "Down")
+turtle.onkeypress(right, "Right")
+turtle.onkeypress(left, "Left")
+turtle.listen()
+
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+
+    if snake.direction == "Up":
+        snake.goto(x_pos, y_pos + SQUARE_SIZE)
+        print("You moved up!")
+    elif snake.direction=="Down":
+        snake.goto(x_pos, y_pos - SQUARE_SIZE)
+    elif snake.direction == "Right":
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print("You moved up!")
+    elif snake.direction == "Left":
+        snake.goto(x_pos - SQUARE_SIZE, y_pos)
+        print("You moved up!")
+ 
+    new_stamp()
+    remove_tail()
+
+    
+
+
+
 turtle.mainloop()
+
+
 
 
 
